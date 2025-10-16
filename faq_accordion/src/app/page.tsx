e080297e486dd75cb66ced6a03867a4b93eea086
Arkhasia <146063272+ArkhasiaHub@users.noreply.github.com>
2025-10-16 12:21:19 +0200

    Add background image
    feat: add background image

--- faq_accordion/src/app/page.tsx
+++ faq_accordion/src/app/page.tsx
@@ -3,8 +3,10 @@ import FaqAccordion from "./components/faq_accordion";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center m-0 p-10 min-h-screen bg-[var(--purple-100)]">
-      <FaqAccordion/>
+    <main className={`flex flex-col justify-center items-center m-0 p-10 min-h-screen bg-[var(--purple-100)] 
+    bg-[url('/images/background-pattern-mobile.svg')] lg:bg-[url('/images/background-pattern-desktop.svg')] 
+    bg-no-repeat bg-contain`}>
+        <FaqAccordion/>
     </main>
   );
 }
