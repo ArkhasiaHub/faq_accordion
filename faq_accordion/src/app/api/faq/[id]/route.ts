49f997f8caf293cf3d65b8a9a38368d94a32c8cf
Arkhasia <146063272+ArkhasiaHub@users.noreply.github.com>
2025-10-16 12:41:20 +0200

    Update route.ts
    feat: update dynamic route.ts

--- faq_accordion/src/app/api/faq/[id]/route.ts
+++ faq_accordion/src/app/api/faq/[id]/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest, context: {params: Promise<{ id: string }>}) {
     const params = await context.params;
     const id = parseInt(params.id, 10);
