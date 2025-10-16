b3642a2787e70a09d7ad9a752c6e5e353f4c6a8e
Arkhasia <146063272+ArkhasiaHub@users.noreply.github.com>
2025-10-16 12:37:31 +0200

    Update get function
    feat: update get function

--- faq_accordion/src/app/api/faq/[id]/route.ts
+++ faq_accordion/src/app/api/faq/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, context: {params:{ id: string }}) {
+export async function GET(request: NextRequest, context: {params: Promise<{ id: string }>}) {
     const params = await context.params;
     const id = parseInt(params.id, 10);
     const faq = await prisma.fAQ.findUnique({ where: {id}});
--- faq_accordion/src/app/api/faq/answer/[id]/route.ts
+++ faq_accordion/src/app/api/faq/answer/[id]/route.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, context: { params: { id: string } }) {
+export async function GET(request: NextRequest, context: { params: Promise<{ id: string }> }) {
     const params = await context.params;
     const id = parseInt(params.id, 10);
-    
+
     const faq = await prisma.fAQ.findUnique({where: {id}, select: {answer: true}});
     if (!faq) {
         return NextResponse.json({error: "Answer not found"});
