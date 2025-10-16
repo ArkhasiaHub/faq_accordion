fd13ea1a10596802488c882790196498f1531ab8
Arkhasia <146063272+ArkhasiaHub@users.noreply.github.com>
2025-10-16 12:34:48 +0200

    Update get function
    feat: update get function

--- faq_accordion/src/app/api/faq/[id]/route.ts
+++ faq_accordion/src/app/api/faq/[id]/route.ts
@@ -2,7 +2,8 @@ import { prisma } from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, context: {params:{ id: string }}) {
-    const id = parseInt(context.params.id, 10);
+    const params = await context.params;
+    const id = parseInt(params.id, 10);
     const faq = await prisma.fAQ.findUnique({ where: {id}});
 
     if (!faq) {
--- faq_accordion/src/app/api/faq/answer/[id]/route.ts
+++ faq_accordion/src/app/api/faq/answer/[id]/route.ts
@@ -2,7 +2,9 @@ import { prisma } from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, context: { params: { id: string } }) {
-    const id = parseInt(context.params.id, 10);
+    const params = await context.params;
+    const id = parseInt(params.id, 10);
+    
     const faq = await prisma.fAQ.findUnique({where: {id}, select: {answer: true}});
     if (!faq) {
         return NextResponse.json({error: "Answer not found"});
